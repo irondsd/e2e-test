dcf81f6d1664824e243f48657d993f153ac19dc1
Konstantin Mednikov <irondsd@gmail.com>
2023-08-30 09:44:45 -0300

    commit formatting

--- spec/babylon.spec.ts
+++ spec/babylon.spec.ts
@@ -1,68 +1,68 @@
 import { test, expect } from '@playwright/test'
 
 test.describe('babylon', async () => {
-    test('babylon title', async ({ page }) => {
-        await page.goto('https://babylon.art/')
+  test('babylon title', async ({ page }) => {
+    await page.goto('https://babylon.art/')
 
-        await expect(page).toHaveTitle('Babylon')
-    })
+    await expect(page).toHaveTitle('Babylon')
+  })
 
-    test('test banner', async ({ page }) => {
-        await page.goto('https://babylon.art/')
+  test('test banner', async ({ page }) => {
+    await page.goto('https://babylon.art/')
 
-        const bannerText = page.locator(
-            ':text("This website uses cookies to ensure you get the best experience on our website.")'
-        )
+    const bannerText = page.locator(
+      ':text("This website uses cookies to ensure you get the best experience on our website.")'
+    )
 
-        await expect(bannerText).toBeOnPage()
-        expect(bannerText).toHaveText('This website uses cookies to ensure you get the best experience on our website.')
+    await expect(bannerText).toBeOnPage()
+    expect(bannerText).toHaveText('This website uses cookies to ensure you get the best experience on our website.')
 
-        const bannerTextHandle = await bannerText.elementHandle()
-        const bannerElement = await bannerTextHandle?.$('xpath=..')
+    const bannerTextHandle = await bannerText.elementHandle()
+    const bannerElement = await bannerTextHandle?.$('xpath=..')
 
-        const okayButton = await bannerElement?.$('button')
+    const okayButton = await bannerElement?.$('button')
 
-        const okayButtonText = await okayButton?.innerText()
+    const okayButtonText = await okayButton?.innerText()
 
-        expect(okayButtonText).toBe('OKAY')
+    expect(okayButtonText).toBe('OKAY')
 
-        await okayButton?.click()
-        await page.waitForTimeout(100)
-        const isElementVisible = await bannerText.isVisible()
+    await okayButton?.click()
+    await page.waitForTimeout(100)
+    const isElementVisible = await bannerText.isVisible()
 
-        expect(isElementVisible).toBeFalsy()
-    })
+    expect(isElementVisible).toBeFalsy()
+  })
 
-    test('test collect button', async ({ page }) => {
-        await page.goto('https://babylon.art/')
+  test('test collect button', async ({ page }) => {
+    await page.goto('https://babylon.art/')
 
-        const buttons = page.locator('div:text("collect editions")')
+    const buttons = page.locator('div:text("collect editions")')
 
-        const [buttonEl] = await buttons.elementHandles()
-        expect(buttonEl).toBeTruthy()
+    const [buttonEl] = await buttons.elementHandles()
+    expect(buttonEl).toBeTruthy()
 
-        await buttonEl.click()
-        expect(page).toHaveURL('https://babylon.art/curated-gallery')
-        expect(page).toHaveTitle('Curated gallery | Babylon')
+    await buttonEl.click()
+    expect(page).toHaveURL('https://babylon.art/curated-gallery')
+    expect(page).toHaveTitle('Curated gallery | Babylon')
 
-        await page.waitForLoadState('load')
+    await page.waitForLoadState('load')
 
-        const pageTitle = page.locator('h1:text("Curated gallery")')
+    const pageTitle = page.locator('h1:text("Curated gallery")')
 
-        await expect(pageTitle).toBeOnPage()
-    })
+    await expect(pageTitle).toBeOnPage()
+  })
 
-    test('go to listing page', async ({ page }) => {
-        await page.goto('https://babylon.art/')
-        await page.getByRole('button', { name: 'collect editions' }).first().click()
-        await page.getByRole('link', { name: 'Ended' }).click()
-        await page.getByRole('link', { name: 'Something Like a Dream You Me and The End of the World' }).click()
+  test('go to listing page', async ({ page }) => {
+    await page.goto('https://babylon.art/')
+    await page.getByRole('button', { name: 'collect editions' }).first().click()
+    await page.getByRole('link', { name: 'Ended' }).click()
+    await page.getByRole('link', { name: 'Something Like a Dream You Me and The End of the World' }).click()
 
-        const title = page.locator('h1:text("Something Like a Dream")')
-        await expect(title).toBeOnPage()
-    })
+    const title = page.locator('h1:text("Something Like a Dream")')
+    await expect(title).toBeOnPage()
+  })
 
-    test('open google', async ({ page }) => {
-        await page.goto('https://google.com')
-    })
+  test('open google', async ({ page }) => {
+    await page.goto('https://google.com')
+  })
 })
